3ff53896da269c300df7ea5346d6f097bd32435f
NghiemLH96 <isaacisc01022014@gmail.com>
2024-08-05 09:03:19 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div style={{backgroundColor: 'red', width: '100%', height: '100vh'}}>
+      <img src="../assets/" alt="" />
+    </div>
+  )
+}
+
+export default App
